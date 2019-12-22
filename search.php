a90c362fd60140c0b699c7f3845078bf0dfd6ae8
flazarte <ferdinandlazarte2017@gmail.com>
2019-12-22 23:31:28 +0800

    search updates form

--- search.php
+++ search.php
@@ -1,14 +1,38 @@
 <?php
 get_header();   
 ?>
-<div id="primary" class="content-area">
-        
-    <div class="container">
-        <div class="search-result-con">
-            <section class="search-con">
-                <h1>Search Result for : <span><?php echo get_search_query(); ?></span></h1>
-                <p class="matches">
-                    <?php
+
+<section class="engine"><a href=""></a></section><section class="cid-rtgokFkwN6" id="header2-5">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(35, 35, 35);"></div>
+
+    <div class="container align-center">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">
+                    Search Results For "<?php echo get_search_query(); ?>" </h1>
+                
+                
+            </div>
+        </div>
+    </div>
+    <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+        <a href="#next">
+            <i class="fas fa-arrow-down"></i>
+        </a>
+    </div>
+</section>
+
+<section class="section-table cid-rtgvspRekY" id="table1-6">
+
+  
+  
+  <div class="container container-table">
+      <h2 class="mbr-section-title mbr-fonts-style align-center pb-3 display-1"><strong>
+
+     <?php
                         // Get total posts
                         if ($wp_query->query['s']) {
                             $total = $wp_query->post_count;
@@ -29,42 +53,60 @@ get_header();
         
                             
                         <?php } ?>
-                        
-                </p>
-            </section> 
-            <?php
+
+      </strong></h2>
+      <div class="table-wrapper">
+        <div class="container">
+          <div class="row search">
+            <div class="col-md-6"></div>
+            <div class="col-md-6">
+            </div>
+          </div>
+        </div>
+
+        <div class="container scroll">
+          <table class="table isSearch" cellspacing="0">
+            <thead class="align-center bold blue">
+              <tr class="table-heads ">
+                  
+                  
+                  
+                  
+              <th class="head-item mbr-fonts-style display-7">Title</th>
+             <th class="head-item mbr-fonts-style display-7">Post Type</th>
+            <th class="head-item mbr-fonts-style display-7">Action</th></tr>
+            </thead>
+
+            <tbody>
+              
+              
+              
+              
+            
+                
+               <?php
                 if ($wp_query->query['s']) {
                 $i = 0;
                 while( have_posts() ): the_post();
                 if ( $i == 0 ) {
                     echo '<section class="search-result"><ul>';
                 }
-            ?>
-            <li>
-                <div class="image-con">
-                    <a href="<?php the_permalink(); ?>">
-                    <?php
-                        $searchImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail' );
-                    ?>
-                    <?php if($searchImg){  ?>
-                        <img src="<?php echo $searchImg[0]; ?>" alt="<?php echo the_title(); ?>" class="search-image">
-                    <?php }else{
-                        if(get_post_type()=='product'){
-                        ?>
-                        
-                        <img src="<?php bloginfo('stylesheet_directory');?>/images/product-placeholder.jpg" alt="Product Image Placeholder">
-                    <?php }} ?>
-                    
-                    </a>
-                </div>
-                <div class="text-con">
-                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                    <span><?php echo get_post_type(); ?></span>
-                </div>
+            ?> 
+               <tr class="align-center">  
                 
-            </li>
-            
-            <?php
+              <td class="body-item mbr-fonts-style display-7"><a target="_blank" href="<?php the_permalink(); ?>" ><?php the_title(); ?></a></td>
+              <?php if(get_post_type()=='product') :?>
+                <td class="body-item mbr-fonts-style display-7">City</td>
+                <?php else :?>
+              <td class="body-item mbr-fonts-style display-7"><?php echo get_post_type(); ?></td>
+          <?php endif ;?>
+              <td class="body-item mbr-fonts-style display-7 blue"><a target="_blank" href="<?php the_permalink(); ?>">VIEW</a></td>
+
+
+              </tr>
+
+
+                <?php
                     $i++;
                     // if we're at the end close the row
                     if ( $i == $total ) {
@@ -79,110 +121,34 @@ get_header();
 
                 }
             ?>
-          
+                
+                
+                
+        </tbody>
+          </table>
         </div>
-    
-       
-        <section class="featured-product">
-            <div class="container">
-                <h3>Featured Products</h3>
-                <div class="slider-con owl-carousel">
+        <div class="container table-info-container">
+          <div class="row info">
+            <div class="col-md-6">
+              <div class="dataTables_info mbr-fonts-style display-7">
+                <span class="infoBefore">Showing</span>
+                <span class="inactive infoRows"></span>
+                <span class="infoAfter">entries</span>
+                <span class="infoFilteredBefore">(filtered from</span>
+                <span class="inactive infoRows"></span>
+                <span class="infoFilteredAfter"> total entries)</span>
+              </div>
+            </div>
+            <div class="col-md-6"></div>
+          </div>
+        </div>
+      </div>
+   
 
-                <?php
-                $query = new WC_Product_Query(array(
-                    'post_type' => 'product_cat',
-                    'product_cat'   => 'featured',                  
-                    'order' => 'ASC'
-                ));
 
             
-                $products = $query->get_products($product_id);
-                    
-
-
-                if (!empty($products)) {
-                foreach ($products as $product) {
-                ?>
-
-
-                    <div class="item on-sale">
-                        <?php if ($product->is_on_sale()) : ?>
-                        <div class="sale-banner">Item on Sale!</div>
-                        <?php else : ?>
-                            <?php endif;  ?>
-                        <div class="item-img">
-                            <a href="<?php echo get_permalink($product->get_id());?>">
-                                <?php if($product->get_image_id()){  ?>
-                                    <img src="<?php echo wp_get_attachment_image_src( $product->get_image_id() ,'medium')[0]; ?>" alt="<?php echo $product->get_name(); ?>" >
-                                <?php }else{?>
-                                    <img src="<?php bloginfo('stylesheet_directory');?>/images/product-placeholder.jpg" alt="Product Image Placeholder">
-                                <?php } ?>
-                            </a>
-                        </div>
-                        <div class="item-body"> 
-
-                            <div class="title"><a href="<?php echo get_permalink($product->get_id());?>"><?php echo $product->get_name(); ?></a></div></a>  
-                        </div>
-
-                    </div>
-                    <?php  } } ?>
-                </div>
-            </div>
-        </section>
-        
-
-        <section class="three-col">
-            <h3>Latest Articles</h3>
-            <div class="owl-carousel" id="latest-articles">
-            <?php query_posts(array(
-                'post_type' => 'post',
-                'orderyby' => 'ASC',
-            ) ); ?>
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <div class="item">
-                <div class="item-img">
-                    <?php 
-                        $size = 'featured-post-img';
-                        $imgHero = get_field('blog_hero'); 
-                        $imgURL = $imgHero['sizes'][$size];
-                        $featuredImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'featured-post-img' );
-                    ?>
-                    <a href="<?php the_permalink(); ?>"><img <?php if (get_the_post_thumbnail_url() == !'') :?>  src="<?= $featuredImg[0]; ?>" <?php else :?> src="<?php echo $imgURL; ?>" <?php endif; ?> alt="<?php the_title(); ?>"></a>
-                </div>
-                <div class="item-body">
-                    <div class="tag-con">
-                        <?php
-                            $category = get_the_category();
-                            $author = get_the_author();
-                        ?>
-                        <p>By: <span><?php the_field('written_by'); ?></span>&nbsp;&#x2022;&nbsp;<span><?php print_r(get_primary_category(get_the_ID()), true); ?></span></p>
-                    </div>
-                    <p class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
-                    <a href="<?php the_permalink(); ?>" class="btn-read-more">Read More</a>
-                    <div class="share-con">
-                        <div class="social-con">
-                            <p>Share:</p>
-                            <ul>
-                                <li>
-                                    <a href="https://www.facebook.com/sharer.php?s=100&u=<?php the_permalink(); ?>" target="_blank" rel="nofollow"><i class="fab fa-facebook"></i></a>
-                                </li>
-                                <li>
-                                    <a href="https://twitter.com/share?url=<?php the_permalink(); ?>" rel="nofollow"><i class="fab fa-twitter"></i></a>
-                                </li>
-                                <li>
-                                    <a href="https://pinterest.com/pin/create/bookmarklet/?url=<?php the_permalink(); ?>" rel="nofollow"><i class="fab fa-pinterest"></i></a>
-                                </li>
-                            </ul>
-                        </div>
-                        
-                    </div>
-                </div>
-            </div>
-            <?php  endwhile; endif; ?>
             </div>
 
         </section>
-    </div>
-</div><!-- #primary -->
 <?php
 get_footer();
